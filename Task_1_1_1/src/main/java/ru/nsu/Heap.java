2cf901acce8bc41c884c00f0e35cfd9ae4c0f73c
noterdamepari <noterdamepari@icloud.com>
2026-09-05 14:09:08 +0700

    Heap base methods implemented

--- Task_1_1_1/src/main/java/ru/nsu/Heap.java
+++ Task_1_1_1/src/main/java/ru/nsu/Heap.java
@@ -13,4 +13,58 @@ public class Heap {
         buffer = new ArrayList<Integer>(cap);
         size = 0;
     }
+
+    private int getParentIdx(int idx){
+        return (idx-1)/2;
+    }
+
+    private void siftUP(int idx){
+        if (idx == 0){
+            return;
+        }
+        int parentIdx = getParentIdx(idx);
+        if (buffer.get(idx) < buffer.get(parentIdx)){
+            swap(idx, parentIdx);
+            siftUP(parentIdx);
+        }
+    }
+
+    private void siftDown(int idx){
+        int lChildIdx = idx*2+1;
+        if (lChildIdx>=size){
+            return;
+        }
+
+        int rChildIdx = idx*2+2;
+        int resChildIdx = lChildIdx;
+        if (rChildIdx < size && buffer.get(rChildIdx) < buffer.get(lChildIdx)){
+            resChildIdx = rChildIdx;
+        }
+
+        swap(idx, resChildIdx);
+        siftDown(resChildIdx);
+    }
+
+    private void swap(int idx1, int idx2){
+        int tmp = buffer.get(idx1);
+        buffer.add(idx1, buffer.get(idx2));
+        buffer.add(idx2, tmp);
+    }
+
+    public void Insert(int value){
+        buffer.add(value);
+        siftUP(size++);
+    }
+
+    public Integer Get(){
+        if (size == 0){
+            System.out.println("panic: heap is empty");
+            return null;
+        }
+        int res = buffer.getFirst();
+        swap(0, --size);
+        buffer.remove(size);
+        siftDown(0);
+        return res;
+    }
 }

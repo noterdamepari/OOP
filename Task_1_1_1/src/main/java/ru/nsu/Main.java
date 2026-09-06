1bfaa317ccd3ac1734dad6d4cc04005ed82d3bb5
noterdamepari <noterdamepari@icloud.com>
2026-09-06 13:05:46 +0700

    Main class implementation, kickstart.sh created

---
+++ Task_1_1_1/src/main/java/ru/nsu/Main.java
@@ -0,0 +1,18 @@
+package ru.nsu;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Amount of nums: ");
+        int num = in.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++){
+            arr[i] = in.nextInt();
+        }
+        arr = Heap.sort(arr);
+        System.out.println(Arrays.toString(arr));
+   }
+}

c8a2ec8dbb26e7b8b14f3862698724348ba64155
noterdamepari <noterdamepari@icloud.com>
2026-09-06 16:14:22 +0700

    small improves

--- Task_1_1_1/src/main/java/ru/nsu/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/Main.java
@@ -3,13 +3,20 @@ package ru.nsu;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
+/**
+ * Main class.
+ */
 public class Main {
+    /**
+     * Program entrypoint.
+     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Amount of nums: ");
         int num = in.nextInt();
         int[] arr = new int[num];
-        for (int i = 0; i < num; i++){
+        for (int i = 0; i < num; i++) {
             arr[i] = in.nextInt();
         }
         arr = Heap.sort(arr);
